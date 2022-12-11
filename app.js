56163e6547ef6f7ded44b741d4673426861396eb
davolpe08 <ashutoshj9508@gmail.com>
2022-12-11 19:26:46 +0530

    Added Port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-
+const port = process.env.PORT || 3000
 const app = express();
 
 app.set('view engine', 'ejs');
